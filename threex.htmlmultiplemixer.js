ca74c34d3ec5e6ba171b9379a62aeacc589d93d0
Jerome Etienne <jerome.etienne@gmail.com>
2014-10-09 14:28:24 +0200

    more work

---
+++ threex.htmlmultiplemixer.js
@@ -0,0 +1,50 @@
+var THREEx = THREEx || {}
+
+
+THREEx.HtmlMultipleMixer	= {}
+
+/**
+ * Multiple context handled as one, thus able to handle multi viewport
+ */
+THREEx.HtmlMultipleMixer.Context	= function(){
+	// store all context
+	var contexts	= []
+	this.contexts	= contexts
+
+	// update all context
+	this.update	= function(){
+		contexts.forEach(function(context){
+			context.update();
+		})
+	}
+}
+
+THREEx.HtmlMultipleMixer.Plane = function(multipleMixerContext, domElement, opts){
+	opts		= opts	|| {}
+	opts		= JSON.parse(JSON.stringify(opts))
+
+	var contexts	= multipleMixerContext.contexts
+	var planes	= []
+	this.planes	= planes
+
+	var mixerContext= contexts[0]
+	var plane 	= new THREEx.HtmlMixer.Plane(mixerContext, domElement, opts);
+	planes.push(plane)
+	this.object3d	= plane.object3d
+
+	opts.object3d	= this.object3d
+
+	for(var i = 1; i < contexts.length; i++){
+		var mixerContext= contexts[i]
+		var cloneElement= domElement.cloneNode()
+		var plane 	= new THREEx.HtmlMixer.Plane(mixerContext, cloneElement, opts);
+		planes.push(plane)
+	}
+
+	// update all context
+	this.update	= function(){
+		contexts.forEach(function(context){
+			context.update();
+		})
+	}
+}
